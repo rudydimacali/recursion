29371208f1615eaf5a5a7032dd71cfe94bf05010
Rudy <rudydimacali@gmail.com>
2018-10-12 13:19:18 -0700

    Completed stringifyJSON.js function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,48 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // stringifiable objects
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return (obj.toString());
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  // unstringifiable objects	
+  } else if (obj === null) {
+    return 'null';
+  }	else if (obj === undefined) {
+    return;
+  } else if (obj.constructor === Function) {
+    return;
+  }
+
+  //Arrays
+  if (obj.constructor === Array) {
+    if (obj.length > 0) {
+      var converted = [];
+      for (var i = 0; i < obj.length; i++) {
+        converted.push(stringifyJSON(obj[i]));
+      }
+      return '[' + converted.join() + ']';
+    }
+    return '[]';
+  }
+
+  if (obj.constructor === Object) {
+    var converted = '';
+    var keys = Object.keys(obj);
+
+    if (keys.length > 0) {
+      for (var i = 0; i < keys.length; i++) {
+        if ((keys[i] === undefined) || (obj[keys[i]] === undefined)) {
+          return '{}';
+        } else if (i === (keys.length - 1)) {
+          converted += stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]);
+        } else {
+          converted += stringifyJSON(keys[i]) + ':' + stringifyJSON(obj[keys[i]]) + ',';
+        }  				
+      }
+      return '{' + converted + '}';
+    }
+    return '{}';
+  }
 };
